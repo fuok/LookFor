4652870e2076d0901f3cd5ce8b08aab0020b85f7
fuok <beast_7th@hotmail.com>
2015-02-05 20:58:38 +0800

    拼接一下接口host

--- src/com/lll/lookfor/HttpInterface.java
+++ src/com/lll/lookfor/HttpInterface.java
@@ -3,21 +3,24 @@ package com.lll.lookfor;
 /** 保存请求的数据接口 */
 public class HttpInterface {
 
+	/** 主机地址 */
+	private static String LOOK_FOR_HOST = "http://182.92.241.249";
+
 	/** 系统登录接口 */
-	public static String SYSTEM_LOGIN = "http://182.92.241.249/lookfor/inter/user/systemLogin";
+	public static String SYSTEM_LOGIN = LOOK_FOR_HOST + "/lookfor/inter/rest/systemLogin";
 
 	/** 获取好友列表 */
-	public static String FRIEND_LIST = "http://www.xshcar.com/chen/friendList.html";
+	public static String FRIEND_LIST = "http://www.xshcar.com/chen/friendList.html";// 假
 
 	/** 获取指定用户地理数据详情 */
-	public static String LBS_LIST = "http://www.xshcar.com/chen/friendDetail.html";
-	
-	/** 用户注册*/
-	public static String REGIST = "http://182.92.241.249/lookfor/inter/user/regist";
-	
-	/**用户登录*/
-	public static String USER_LOGIN = "http://182.92.241.249/lookfor/inter/user/userLogin";
-	
-	/**修改昵称*/
-	public static String MODIFY_NICKNAME = "http://182.92.241.249/lookfor/inter/user/modifyNickName";
+	public static String LBS_LIST = "http://www.xshcar.com/chen/friendDetail.html";// 假
+
+	/** 用户注册 */
+	public static String REGIST = LOOK_FOR_HOST + "/lookfor/inter/user/regist";
+
+	/** 用户登录 */
+	public static String USER_LOGIN = LOOK_FOR_HOST + "/lookfor/inter/user/userLogin";
+
+	/** 修改昵称 */
+	public static String MODIFY_NICKNAME = LOOK_FOR_HOST + "/lookfor/inter/user/modifyNickName";
 }
